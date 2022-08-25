12bb2243ff6ff6a6a332bb4d9eb1d1e03fedc2ee
kuzcode <igormakw228@gmail.com>
2022-08-25 03:38:54 +0300

    Create IDictionaryDebugView proxy.

---
+++ src/BidirectionalDictionary/DictionaryDebugView.cs
@@ -0,0 +1,29 @@
+﻿using System.Diagnostics;
+
+namespace System.Collections.Generic
+{
+    internal sealed class IDictionaryDebugView<TKey, TValue>
+        where TKey : notnull
+        where TValue : notnull
+    {
+        private IDictionary<TKey, TValue> _dictionary;
+
+        public IDictionaryDebugView(IDictionary<TKey, TValue> dictionary)
+        {
+            _dictionary = dictionary ?? throw new ArgumentNullException(nameof(dictionary));
+        }
+
+        [DebuggerBrowsable(DebuggerBrowsableState.RootHidden)]
+        public KeyValuePair<TKey, TValue>[] Items
+        {
+            get
+            {
+                var items = new KeyValuePair<TKey, TValue>[_dictionary.Count];
+
+                _dictionary.CopyTo(items, 0);
+
+                return items;
+            }
+        }
+    }
+}
