d672bb0adf823c4bd1a1399ebb707543757f60ab
Ihor Kuzmychov <iikuzmychov@gmail.com>
2026-03-07 22:44:12 +0100

    chore: namespaces

--- src/BidirectionalDictionary/DictionaryDebugView.cs
+++ src/BidirectionalDictionary/DictionaryDebugView.cs
@@ -1,27 +1,26 @@
 ﻿using System.Diagnostics;
 
-namespace System.Collections.Generic
+namespace System.Collections.Generic;
+
+internal sealed class DictionaryDebugView<TKey, TValue>
+    where TKey : notnull
 {
-    internal sealed class DictionaryDebugView<TKey, TValue>
-        where TKey : notnull
+    private readonly IDictionary<TKey, TValue> _dictionary;
+
+    public DictionaryDebugView(IDictionary<TKey, TValue> dictionary)
     {
-        private readonly IDictionary<TKey, TValue> _dictionary;
+        _dictionary = dictionary ?? throw new ArgumentNullException(nameof(dictionary));
+    }
 
-        public DictionaryDebugView(IDictionary<TKey, TValue> dictionary)
+    [DebuggerBrowsable(DebuggerBrowsableState.RootHidden)]
+    public KeyValuePair<TKey, TValue>[] Items
+    {
+        get
         {
-            _dictionary = dictionary ?? throw new ArgumentNullException(nameof(dictionary));
-        }
+            var items = new KeyValuePair<TKey, TValue>[_dictionary.Count];
+            _dictionary.CopyTo(items, 0);
 
-        [DebuggerBrowsable(DebuggerBrowsableState.RootHidden)]
-        public KeyValuePair<TKey, TValue>[] Items
-        {
-            get
-            {
-                var items = new KeyValuePair<TKey, TValue>[_dictionary.Count];
-                _dictionary.CopyTo(items, 0);
-
-                return items;
-            }
+            return items;
         }
     }
 }
